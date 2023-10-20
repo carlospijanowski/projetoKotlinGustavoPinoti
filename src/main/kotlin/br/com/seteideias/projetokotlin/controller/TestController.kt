90447e51310a54a3373e4dedbd997197495cc522
Pijanowski <carlos.pijanowski@gmail.com>
2023-10-20 13:37:43 -0300

    20231020 1338

--- src/main/kotlin/br/com/seteideias/projetokotlin/controller/TestController.kt
+++ src/main/kotlin/br/com/seteideias/projetokotlin/controller/TestController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,7 +18,10 @@ class TestController {
     var customers = mutableListOf<Pessoa>()
 
     @GetMapping
-    fun getPessoasList() : List<Pessoa> {
+    fun getPessoasList(@RequestParam name : String?) : List<Pessoa> {
+        name?.let {
+            return customers.filter { it.nome.contains(name,true) }
+        }
         return customers
     }
 
